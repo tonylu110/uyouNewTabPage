2a0435de61af80bb6d6f5b9a8576d40d2812de67
托噜酱 <58923223+tonylu110@users.noreply.github.com>
2022-05-21 11:29:21 +0800

    first commit

---
+++ src/components/Background.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../scss/Background.scss'
+
+export default class Background extends Component {
+  render() {
+    return (
+      <div className='background' style={{backgroundImage: 'url(https://iw233.cn/api.php?sort=pc)'}}>
+          
+      </div>
+    )
+  }
+}
