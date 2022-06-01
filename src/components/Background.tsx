46bfd9ba88a4930f29e7211324a94e2cf19957f0
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2022-06-01 16:46:01 +0800

    Refactor: background load

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -7,38 +7,41 @@ import isHideAll from "../util/isHideAll";
 import mobileCheck from "../util/isMobile";
 
 export default class Background extends Component<any, any> {
+  private readonly mobileCheck: mobileCheck = new mobileCheck()
   state = {
-    isMobile: new mobileCheck().isMobile(),
+    isMobile: this.mobileCheck.isMobile(),
     backgroundShow: false,
-    background: new mobileCheck().getBackground().backgroundImage,
-    backgroundHeight: new mobileCheck().getBackground().backgroundHeight,
+    background: this.mobileCheck.getBackground().backgroundImage,
+    backgroundHeight: this.mobileCheck.getBackground().backgroundHeight,
     hideAll: isHideAll()
   }
   render() {
-    let img: HTMLImageElement = document.createElement('img');
-    img.src = this.state.isMobile ? 'https://dev.iw233.cn/api.php?sort=mp' : 'https://dev.iw233.cn/api.php?sort=pc';
-    img.onload = () => {
-      this.setState({
-        backgroundShow: true
-      })
-    }
     return (
-      <div 
-        className='background' 
+      <div
+        className='background'
         style={{
           display: this.state.backgroundShow ? '' : 'none',
           backgroundImage: this.state.background,
           height: this.state.backgroundHeight
         }}
       >
-        <MainArea hideAll={this.state.hideAll} />
-        {this.state.hideAll ? null : <One />}
+        <MainArea hideAll={this.state.hideAll}/>
+        {this.state.hideAll ? null : <One/>}
         <RightTopButtons hideAll={(e: boolean) => {
           this.setState({
             hideAll: e
           })
-        }} />
+        }}/>
       </div>
     )
+  }
+  componentDidMount() {
+    let img = new Image()
+    img.src = this.state.background.slice(4, -1)
+    img.onload = () => {
+      this.setState({
+        backgroundShow: true
+      })
+    }
   }
 }
